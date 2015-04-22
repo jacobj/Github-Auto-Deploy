a6c4f14b0f454883ca3f6a6704c31fc485a5a547
Jacob Jenkins <jacob@jacobejenkins.com>
2015-04-21 20:06:15 -0400

    i

--- GitAutoDeploy.py
+++ GitAutoDeploy.py
@@ -74,7 +74,7 @@ class GitAutoDeploy(BaseHTTPRequestHandler):
         if(not self.quiet):
             print "\nPost push request received"
             print 'Updating ' + path
-        call(['cd "' + path + '" && git fetch'], shell=True)
+        call(['cd "' + path + '" && git fetch; git reset --hard origin/master'], shell=True)
 
     def deploy(self, path):
         config = self.getConfig()
